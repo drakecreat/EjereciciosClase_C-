2db06d946e95422d7bdc1de2ad84f6810f6d358e
drake <drake.creat@gmail.com>
2026-05-23 02:04:08 -0500

    Create a program for user input of noun and verb
    Add a program that takes a noun and a verb as input and displays a sentence using them.

---
+++ Semana7.cpp
@@ -0,0 +1,30 @@
+#include   <iostream>
+using namespace std;
+int main() {
+    string sustantivo, verbo;
+    cout << "Ingrese un sustantivo: "<<endl;
+    cin >> sustantivo;
+    cout << "Ingrese un verbo: "<<endl;
+    cin >> verbo;
+    cout <<"El "<<sustantivo<<" decidio "<<verbo <<" en la luna"<< endl;
+
+    return 0;
+}
+# include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hola \nMundo \n!" << endl;
+
+    return 0;
+}
+# include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
